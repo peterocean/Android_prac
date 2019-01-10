0d05bf395ce4f8fc3d7f30c90b60a56ece9451cc
yangyongfeng <yangyongfeng@zhkh.com>
2019-01-10 15:33:19 +0800

    1. UI练习   1.1 UI增加ImageVies练习

--- app/UITest/app/src/main/java/com/example/administrator/uitest/MainActivity.java
+++ app/UITest/app/src/main/java/com/example/administrator/uitest/MainActivity.java
@@ -5,22 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
+    private  ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button)findViewById(R.id.button_1);
+        Button button = (Button) findViewById(R.id.button_1);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView)findViewById(R.id.image_View);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inputText = editText.getText().toString();
-                Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();
+                imageView.setImageResource(R.mipmap.android9_pie);
             }
         });
     }
