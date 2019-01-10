f30df706eec0de7dea2e4db27e0feaec03a17e46
yangyongfeng <yangyongfeng@zhkh.com>
2019-01-10 10:00:50 +0800

    1.UI练习   1.1增加EditText测试

--- app/UITest/app/src/main/java/com/example/administrator/uitest/MainActivity.java
+++ app/UITest/app/src/main/java/com/example/administrator/uitest/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.administrator.uitest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button_1);
+        editText = (EditText) findViewById(R.id.edit_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
