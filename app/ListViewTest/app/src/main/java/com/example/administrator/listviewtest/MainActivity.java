51dff9a4af001820fd9d8468022b3e8617acf249
yangyongfeng <yangyongfeng@zhkh.com>
2019-01-17 17:50:40 +0800

    1.ListView优化

--- app/ListViewTest/app/src/main/java/com/example/administrator/listviewtest/MainActivity.java
+++ app/ListViewTest/app/src/main/java/com/example/administrator/listviewtest/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.administrator.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         FruitAdapter adapter = new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this,fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     private void initFruit(){
         for (int i = 0; i < 2; i++){
