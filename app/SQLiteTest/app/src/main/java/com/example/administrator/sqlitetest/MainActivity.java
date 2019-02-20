ecc69202cac5a5c9b350c17e911d8e1a0cd890bc
yangyongfeng <yangyongfeng@zhkh.com>
2019-02-20 16:26:44 +0800

    1. SqLite测试   1.1 增加数据查询操作

--- app/SQLiteTest/app/src/main/java/com/example/administrator/sqlitetest/MainActivity.java
+++ app/SQLiteTest/app/src/main/java/com/example/administrator/sqlitetest/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.administrator.sqlitetest;
 import android.Manifest;
 import android.content.ContentValues;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -68,6 +70,28 @@ public class MainActivity extends AppCompatActivity {
                 ContentValues values = new ContentValues();
                 values.put("price",10.99);
                 db.update("Book",values,"name = ?", new String[]{"The Da Vinci Code"});
+            }
+        });
+
+        Button queryDataButton = (Button)findViewById(R.id.qurey_data);
+        queryDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = myDatabaseHelper.getWritableDatabase();
+                Cursor cursor = db.query("Book",null,null,null,null,null,null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity","book name is " + name );
+                        Log.d("MainActivity","book author is " + author );
+                        Log.d("MainActivity","book pages is " + pages );
+                        Log.d("MainActivity","book price is " + price );
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
